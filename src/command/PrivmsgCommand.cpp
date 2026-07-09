ada847da342151e137f86cffbf72dc883fb8e221
Enzo <enzochevrier@icloud.com>
2026-07-09 05:41:37 +0200

    fix: privmsg was not good vasy j'ai la flemme il est 5h40

--- src/command/PrivmsgCommand.cpp
+++ src/command/PrivmsgCommand.cpp
@@ -18,38 +18,43 @@ PrivmsgCommand::~PrivmsgCommand() {}
 
 void PrivmsgCommand::execute() const
 {
-	std::vector<std::string> 	target = this->_params.front();
+	std::vector<std::string> 	targets = this->_params.front();
 	std::string 				msgContent = this->_trailer;
 
-	if (target[0][0] == '#')
+	for (size_t i = 0; i < targets.size(); i++)
 	{
-		std::map<std::string, Channel *> const &m = this->_server->getChannelMap();
-		if (m.end() == m.find(target.front()))
-		{
-			std::string reply = this->_director.errNoSuchChannel(this->_client->getNickname(), target.front());
-			this->_server->writeInBuffer(this->getClient(), reply);
-			return ;
-		}
+		std::string const &target = targets[i];
 
-		Channel *chan = m.find(target.front())->second;
-		if (chan->isUserInChannel(this->_client->getFd()))
-			chan->sendMessageToAllOther(this->_director.rplPrivmsg(*this->_client, *chan, msgContent), this->_client->getFd());
+		if (target[0] == '#')
+		{
+			std::map<std::string, Channel *> const &m = this->_server->getChannelMap();
+			if (m.end() == m.find(target))
+			{
+				std::string reply = this->_director.errNoSuchChannel(this->_client->getNickname(), target);
+				this->_server->writeInBuffer(this->getClient(), reply);
+				continue ;
+			}
+
+			Channel *chan = m.find(target)->second;
+			if (chan->isUserInChannel(this->_client->getFd()))
+				chan->sendMessageToAllOther(this->_director.rplPrivmsg(*this->_client, *chan, msgContent), this->_client->getFd());
+			else
+			{
+				std::string reply = this->_director.errNotOnChannel(this->_client->getNickname(), target);
+				this->_server->writeInBuffer(this->getClient(), reply);
+			}
+		}
 		else
 		{
-			std::string reply = this->_director.errNotOnChannel(this->_client->getNickname(), target.front());
-			this->_server->writeInBuffer(this->getClient(), reply);
-		}	
-	}
-	else
-	{
-		Client *targetClient = this->_server->getClient(target.front());
-		if (targetClient == NULL)
-		{
-			std::string reply = this->_director.errNoSuchNick(this->_client->getNickname(), target.front());
-			this->_server->writeInBuffer(this->getClient(), reply);
-			return ;
+			Client *targetClient = this->_server->getClient(target);
+			if (targetClient == NULL)
+			{
+				std::string reply = this->_director.errNoSuchNick(this->_client->getNickname(), target);
+				this->_server->writeInBuffer(this->getClient(), reply);
+				continue ;
+			}
+			std::string reply = this->_director.rplPrivmsg(*this->_client, *targetClient, msgContent);
+			this->_server->writeInBuffer(targetClient, reply);
 		}
-		std::string reply = this->_director.rplPrivmsg(*this->_client, *targetClient, msgContent);
-		this->_server->writeInBuffer(targetClient, reply);
 	}
 }
